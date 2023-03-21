850da7cb6c3f64c7ec60ddc802ca98ce311310a9
stephan-chiorean <112440764+stephan-chiorean@users.noreply.github.com>
2023-03-20 20:49:51 -0700

    let's see if this works

--- .history/components/Skill_20230320204908.tsx
+++ .history/components/Skill_20230320204908.tsx
@@ -76,13 +76,13 @@ function Skill({directionLeft, skill}: Props) {
             className="rounded-full border-2 border-[#F7AB0A] bg-white/80 object-contain p-1 h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale ease-in-out"
           />
         }
-        <div className="absolute opacity-0 hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-[#F7AB0A] h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+        {shouldRenderPercent && <div className="absolute opacity-0 hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-[#F7AB0A] h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
           <div className="flex items-center justify-center h-full">
             <p className="text-3xl font-bold text-black opacity-100">
               {progress[skill]}%
             </p>
           </div>
-        </div>
+        </div>}
       </div>
     )
   }
